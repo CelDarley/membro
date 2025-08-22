df3c4004201b8209c50d5c8b5c63d66704768c2f
CelDarley <coroneldarley@gmail.com>
2025-08-21 23:55:34 -0300

    fix: remover refs a testimonials/plans/faqs do SiteSettingController e retornar 401 JSON no Authenticate

--- back/app/Http/Middleware/Authenticate.php
+++ back/app/Http/Middleware/Authenticate.php
@@ -12,6 +12,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        // Em APIs, preferimos 401 JSON em vez de redirecionamento
+        return null;
     }
 }
